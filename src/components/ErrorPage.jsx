e9c71142e0698d95daf4538b2cd0672ecc3e24a4
Coker <gbolahancoker99@gmail.com>
2024-05-20 12:01:57 +0100

    update

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
-
+import Nav from './HomeScreen/Nav'
+import { Link } from 'react-router-dom';
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoGithub } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <div className='error'>
+        <Nav />
+        <div className="error-page">
+            <h1>Oops!</h1>
+            <p>The page you visited does not exist</p>
+            <div className="error_home">
+                <p>Navigate back to</p>
+                <Link to={"/"}>home page</Link>
+            </div>
+        </div>
+        <div className='error-footer'> 
+            <div className='error-logo'>
+                <Link to={"https://www.instagram.com/sckokyy/"} target='_blank'> 
+                    <FaInstagram />
+                </Link>
+                <Link to={"https://twitter.com/Sckokyy"} target='_blank'>
+                    <FaXTwitter />
+                </Link>
+                <Link to={'https://github.com/GbollySckoky'} target='_blank'>
+                    <IoLogoGithub />
+                </Link>
+                <Link to={"https://www.linkedin.com/in/gbolahan-coker-85ba39241/"} target='_blank'>
+                    <FaLinkedin />
+                </Link>
+            </div>
+            <div className='error_copy'>
+                <p> &copy; 2024</p>
+                <Link to={'mailto:gbolahancoker99@gmail.com'} target='_blank'>gbolahancoker99@gmail.com</Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 import Nav from '../HomeScreen/Nav';
 import Avatar from '@mui/material/Avatar';
-import Footer from '../Footer/Footer';
+import {Link} from 'react-router-dom'
 import { MdKeyboardArrowRight } from "react-icons/md";
 import david from '../../assets/david.jpg'
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoGithub } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
 import Modal from './Modal';
+
+
 const Profile = ({user}) => {
 const [showModal, setShowModal] = useState(false)
 const handleShowModal = () => {
@@ -18,7 +24,7 @@ const handleShowModal = () => {
           <h1>Edit Profile</h1>
           <div  className='profile_info'>
             <div className='profile_details'>
-                <Avatar alt="Remy Sharp" src={david} />
+                <Avatar id="imgs" alt="Remy Sharp" src={david} />
                 <h2>{user.email}</h2>
             </div>
           </div>
@@ -30,7 +36,7 @@ const handleShowModal = () => {
           <div className='profile_member'>
             <p>Member since May 2024</p>
             <div className='plan'>
-              <h3>Mobile Plan</h3>
+              <h3>Movie Plan</h3>
               <p>Next Payment: Unlimited Subscription</p>
               <p id="yu">{user.email}</p>
             </div>
@@ -46,8 +52,25 @@ const handleShowModal = () => {
           </div>
         </div>
         </div>
-        <div className="profile_footer">
-        <Footer />
+        <div className='profile-footer'> 
+            <div className='profile-logo'>
+                <Link to={"https://www.instagram.com/sckokyy/"} target='_blank'> 
+                    <FaInstagram />
+                </Link>
+                <Link to={"https://twitter.com/Sckokyy"} target='_blank'>
+                    <FaXTwitter />
+                </Link>
+                <Link to={'https://github.com/GbollySckoky'} target='_blank'>
+                    <IoLogoGithub />
+                </Link>
+                <Link to={"https://www.linkedin.com/in/gbolahan-coker-85ba39241/"} target='_blank'>
+                    <FaLinkedin />
+                </Link>
+            </div>
+            <div className='profile_copy'>
+                <p> &copy; 2024</p>
+                <Link to={'mailto:gbolahancoker99@gmail.com'} target='_blank'>gbolahancoker99@gmail.com</Link>
+            </div>
         </div>
       </div>
     </div>
