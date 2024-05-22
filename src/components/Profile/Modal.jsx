e5f86656e471fe10885a79e63f3fcc91401b2c65
Coker <gbolahancoker99@gmail.com>
2024-05-22 08:57:47 +0100

    up

--- src/components/Profile/Modal.jsx
+++ src/components/Profile/Modal.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Modal = ({handleShowModal}) => {
-    const beer = ''
+
   return (
     <div className='modal-background' onClick={handleShowModal}>
         <div className="modal-content">
